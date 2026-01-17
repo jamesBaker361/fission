d259f1f9a708a05440c759fc52359b36b59bc521
James Baker <jlbaker361@gmail.com>
2026-01-17 15:46:52 -0500

    shape

--- shape_test.py
+++ shape_test.py
@@ -1,7 +1,5 @@
 import datasets
-
-data=datasets.load_dataset("ShapeNet/ShapeNetCore",split="train")
-
-for row in data:
-    print(row)
-    break
+import os
+from huggingface_hub import snapshot_download
+os.makedirs("shapenet",exist_ok=True)
+snapshot_download(repo_id="gpt2", allow_patterns=["*.zip"], local_dir="shapenet")

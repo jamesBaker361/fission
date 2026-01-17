f1bfef76652922415ad139b5e7f79bae49af73ad
James Baker <jlbaker361@gmail.com>
2026-01-16 20:03:55 -0500

    download

--- download_aws.py
+++ download_aws.py
@@ -1,20 +1,24 @@
 import requests
 from tqdm import tqdm
+#https://amazon-berkeley-objects.s3.amazonaws.com/archives/abo-listings.tar
 
-url = "https://amazon-berkeley-objects.s3.amazonaws.com/archives/abo-3dmodels.tar"
-output_file = "abo-3dmodels.tar"
-response = requests.get(url, stream=True)
-total_size = int(response.headers.get("content-length", 0))
 
-with open(output_file, "wb") as f, tqdm(
-    desc=output_file,
-    total=total_size,
-    unit="iB",
-    unit_scale=True,
-    unit_divisor=1024,
-) as bar:
-    for chunk in response.iter_content(chunk_size=8192):
-        f.write(chunk)
-        bar.update(len(chunk))
+for output_file in ['abo-listings.tar','abo-3dmodels.tar']:
+    
+    url = f"https://amazon-berkeley-objects.s3.amazonaws.com/archives/{output_file}"
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get("content-length", 0))
 
-print("Download finished!")
+    with open(output_file, "wb") as f, tqdm(
+        desc=output_file,
+        total=total_size,
+        unit="iB",
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as bar:
+        for chunk in response.iter_content(chunk_size=8192):
+            f.write(chunk)
+            bar.update(len(chunk))
+
+    print(f"Downloading {output_file} finished!")
+    break

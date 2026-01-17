88b5160d18bcf327f37c0c798785a08131a4094e
James Baker <jlbaker361@gmail.com>
2026-01-17 15:56:24 -0500

    h

--- shape_test.py
+++ shape_test.py
@@ -2,4 +2,4 @@ import datasets
 import os
 from huggingface_hub import snapshot_download
 os.makedirs("shapenet",exist_ok=True)
-snapshot_download(repo_id="gpt2", allow_patterns=["*.zip"], local_dir="shapenet")
+snapshot_download(repo_id="ShapeNet/ShapeNetCore", allow_patterns=["*.zip"], local_dir="shapenet")

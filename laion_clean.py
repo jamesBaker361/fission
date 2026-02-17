907a12061e1be49564c9c242b42fd09790e9ff37
James Baker <jlbaker361@gmail.com>
2026-02-17 00:59:33 -0500

    laion

---
+++ laion_clean.py
@@ -0,0 +1,14 @@
+import os
+from PIL import Image
+
+
+with open(os.path.join("laion","captions.csv"),"rt") as file:
+    with open(os.path.join("laion","clean_captions.csv","wt")) as caption_file:
+        for r,row in enumerate(file.readlines()):
+            row=row.split(",")
+            if os.path.exists(row[0]):
+                try:
+                    Image.open(row[0]).convert("RGB")
+                    caption_file.write(f"{row}\n")
+                except:
+                    pass
